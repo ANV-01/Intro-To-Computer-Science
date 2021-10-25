bf8ce1a2b3b89f0b1e3c9c2e1c1a779ccae5606d
ANV-01 <92701486+ANV-01@users.noreply.github.com>
2021-10-25 02:59:22 -0700

    Add files via upload

---
+++ Ex4-while-loop-1.py
@@ -0,0 +1,6 @@
+#Andy Villegas
+count = 0
+userinput = int(input("Enter a numeric value of how many @'s you want:")) #asks for how man @ symbols the user wants and proceedes to multiply it
+while count <= userinput:
+    print("@"*count)
+    count = count+1
