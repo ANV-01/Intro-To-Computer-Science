d347947e0b58baa9f9d1ddb080593b873ac1805b
ANV-01 <92701486+ANV-01@users.noreply.github.com>
2021-10-25 02:53:10 -0700

    Add files via upload

---
+++ Ex1-List-lab.py
@@ -0,0 +1,65 @@
+#Andy Villegas
+var1 = "A Red Fox"
+var2 = "chased a Blue mouse"
+var3 = "across a Green meadow"
+
+print(var1, var2, var3) #prints var 1-3 in 1 line
+print('-----------------------------')
+var1 = "A Red Fox"
+var2 = " chased a Blue mouse "
+var3 = "across a Green meadow"
+var4 = var1 + var2 + var3
+#prints var 1-3 in a line but makes it easier to write the print statement
+print(var4)
+print('-----------------------------')
+var1 = "A Red Fox"
+var2 = " chased a Blue mouse "
+var3 = "across a Green meadow"
+var4 = var1 + var2 + var3
+
+print(var4.lower()) #makes var4, which is var1-3 combined, lowercase
+print(var4)
+print('-----------------------------')
+var1 = "A Red Fox"
+var2 = " chased a Blue mouse "
+var3 = "across a Green meadow"
+var4 = var1 + var2 + var3
+
+print(var4.upper())  #makes var4, which is var1-3 combined, uppercase
+print(var4)
+print('-----------------------------')
+var1 = "A Red Fox"
+var2 = " chased a Blue mouse "
+var3 = "across a Green meadow"
+var4 = var1 + var2 + var3
+
+print(var4)
+print(var4[19]) #finds the 19th position in the string
+print('-----------------------------')
+var1 = "A Red Fox"
+var2 = " chased a Blue mouse "
+var3 = "across a Green meadow"
+var4 = var1 + var2 + var3
+
+print(var4)
+print(var4[19:23]) #finds the 19-23rd positions in the string
+print('-----------------------------')
+var1 = "A Red Fox"
+var2 = " chased a Blue mouse "
+var3 = "across a Green meadow"
+var4 = var1 + var2 + var3
+
+print(var4)
+print(len(var4)) #shows the length of the string
+print('-----------------------------')
+var1 = "A Red Fox"
+var2 = " chased a Blue mouse "
+var3 = "across a Green meadow"
+var4 = var1 + var2 + var3
+
+print(var4.isnumeric()) #determines if the string is made of numbers or not
+print('-----------------------------')
+var5 = "621"
+
+print(var5)
+print(var5.isnumeric()) 
