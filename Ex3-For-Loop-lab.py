d4f1bbbb1567d358f5b878d3443dd14d556be23b
ANV-01 <92701486+ANV-01@users.noreply.github.com>
2021-10-25 02:56:09 -0700

    Add files via upload

---
+++ Ex3-For-Loop-lab.py
@@ -0,0 +1,48 @@
+#Andy Villegas
+largest_number = 0
+largest_found = 0
+total_sum = 0
+total_average = 0 #variables defined to be used for the rest of the program
+my_list = [4, 76, 2, 234, 9, 71]
+for my_list2 in my_list:
+    print(my_list2, end=" ")
+    if my_list2 > largest_found:
+        total_sum = sum(my_list)
+        total_average = sum(my_list)/len(my_list)  #gets the total average of the numbers
+        largest_found = my_list2
+        largest_number = my_list2
+print("\nSum =", total_sum)
+print("Average =", total_average)
+print("The Largest Number =", largest_number)
+print("\n----------------")
+largest_number = 0
+largest_found = 0
+total_sum = 0
+total_average = 0 #variables defined to be used for the rest of the program
+my_list = [3, -17, 99, -32, 321, 62, 27]
+for my_list2 in my_list:
+    print(my_list2, end=" ")
+    if my_list2 > largest_found:
+        total_sum = sum(my_list)
+        total_average = sum(my_list)/len(my_list) #gets the total average of the numbers
+        largest_found = my_list2
+        largest_number = my_list2
+print("\nSum =", total_sum)
+print("Average =", total_average)
+print("The Largest Number =", largest_number)
+print("\n----------------")
+largest_number = 0
+largest_found = 0
+total_sum = 0
+total_average = 0 #variables defined to be used for the rest of the program
+my_list = [1899, 1978, 23, 0, 23, 37, 237, 8, 2, -77, 31]
+for my_list2 in my_list:
+    print(my_list2, end=" ")
+    if my_list2 > largest_found:
+        total_sum = sum(my_list)
+        total_average = sum(my_list)/len(my_list) #gets the total average of the numbers
+        largest_found = my_list2
+        largest_number = my_list2
+print("\nSum =", total_sum)
+print("Average =", total_average)
+print("The Largest Number =", largest_number)
