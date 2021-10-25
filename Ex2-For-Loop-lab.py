bc66b24105e052838f62fc4354c33f81370db133
ANV-01 <92701486+ANV-01@users.noreply.github.com>
2021-10-25 02:53:54 -0700

    Add files via upload

---
+++ Ex2-For-Loop-lab.py
@@ -0,0 +1,39 @@
+#Andy Villegas
+my_list = ['earth', 'exciting', 'day', 'laptop', 'electrochemical', 'blue', 'up']
+largest_word = ''
+largest_found = 0
+for my_list2 in my_list:
+    print(my_list2, end=" ")
+    len_of_string = len(my_list2)
+    print(len_of_string) #gets the length of my_list2 and prints it
+    word_len = len(my_list2)
+    if word_len > largest_found:
+        largest_found = word_len
+        largest_word = my_list2
+print("Longest word is", largest_word) 
+print("\n----------------")
+my_list = ['indistinguishable', 'acorn', 'soap', 'ladder', 'cheese', 'disillusioned', 'pit', 'monday', 'placeholder', 'teleconference']
+largest_word = ''
+largest_found = 0
+for my_list2 in my_list:
+    print(my_list2, end=" ")
+    len_of_string = len(my_list2)
+    print(len_of_string) #gets the length of my_list2 and prints it
+    word_len = len(my_list2)
+    if word_len > largest_found:
+        largest_found = word_len
+        largest_word = my_list2
+print("Longest word is", largest_word)
+print("\n----------------")
+my_list = ['fixture', 'dangerous', 'humanitarianism', 'trip', 'orange', 'cat', 'triskaidekaphobia', 'nominal', 'and', 'atomic']
+largest_word = ''
+largest_found = 0
+for my_list2 in my_list:
+    print(my_list2, end=" ")
+    len_of_string = len(my_list2)
+    print(len_of_string) #gets the length of my_list2 and prints it
+    word_len = len(my_list2)
+    if word_len > largest_found:
+        largest_found = word_len
+        largest_word = my_list2
+print("Longest word is", largest_word)
