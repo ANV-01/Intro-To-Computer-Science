40e2ff7a0ebb9b6389d91b5f1145173c888ba51d
ANV-01 <92701486+ANV-01@users.noreply.github.com>
2021-10-25 02:50:58 -0700

    Add files via upload

---
+++ Ex1-GUI-01-lab.py
@@ -0,0 +1,9 @@
+#Andy Villegas
+import tkinter
+
+monday = tkinter.Tk()
+monday.geometry('300x300')
+monday.configure(bg="red")
+monday.title("Whole Lotta Red") #makes a GUI window with a red background titled Whole Lotta Red
+
+monday.mainloop()
