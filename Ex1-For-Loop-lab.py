72324bd3072c3d0d2b364329efba4c2d5414dc29
ANV-01 <92701486+ANV-01@users.noreply.github.com>
2021-10-25 02:50:12 -0700

    Add files via upload

---
+++ Ex1-For-Loop-lab.py
@@ -0,0 +1,15 @@
+#Andy Villegas
+for numbers in range(1, 16): #lists numbers 1-16
+    print(numbers, end=" ")
+print("\n----------------")
+for numbers in range(10, 26): #lists numbers 10-26
+    print(numbers, end=" ")
+print("\n----------------")
+for numbers in range(7, 30, +4): #lists numbers 7-30 by increments of 4
+    print(numbers, end=" ")
+print("\n----------------")
+for numbers in range(23, 7, -3): #lists every 3rd number going down the list 23-7
+    print(numbers, end=" ")
+print("\n----------------")
+for numbers in range(2, 21, +2): #lists numbers 2-21 by increments of 2
+    print(numbers, end=" ")
