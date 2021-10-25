3adc484c86d20e4444dc60e6e3abde5b54acb004
ANV-01 <92701486+ANV-01@users.noreply.github.com>
2021-10-25 02:57:59 -0700

    Add files via upload

---
+++ Ex3-while-loop-1.py
@@ -0,0 +1,10 @@
+#Andy Villegas
+subtotal = 0
+
+entry = input("Please enter a number to add or 'q' to quit >: ") #asks for user input
+while entry != "q":
+    subtotal = (subtotal + int(entry)) #adds the subtotal with the user input
+    print("Your Sub total is", subtotal)
+    entry = input("Please enter a number to add or 'q' to quit >: ")
+
+print("Your Grand total is", subtotal)
